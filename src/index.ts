ee41fb7f5273765a7f98e65575522c3d2df2cea6
Pedro Moreno <nennogabriel@gmail.com>
2024-09-04 12:40:22 -0300

    feat: add github libraries to work with actions and setup the bundler

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world");
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+
+export async function run() {
+    try {
+        const description = github.context.payload.pull_request?.body;
+        if (!description) {
+            core.setFailed('No description found for this pull request.');
+            return;
+        }
+
+    } catch (error) {
+        core.setFailed((error as Error).message);
+    }   
+}
+
+run();
