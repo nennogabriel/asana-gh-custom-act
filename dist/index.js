f38d4c32d55a733d3631f3c2a42e6a8d1836f5b8
Pedro Moreno <nennogabriel@gmail.com>
2024-09-04 22:24:22 -0300

    build: add `prettier`, `eslint` and `lint-staged`

--- dist/index.js
+++ dist/index.js
@@ -35,33 +35,37 @@ const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const Asana = __importStar(__nccwpck_require__(576));
 async function run() {
-    try {
-        const token = core.getInput('asana-token');
-        console.log('token', token);
-        const client = Asana.Client.create().useAccessToken(token);
-        const pr = github.context.payload.pull_request;
-        if (!pr) {
-            core.setFailed('No pull request found');
-            return;
-        }
-        const description = pr.body;
-        if (!description) {
-            core.setFailed('No description found for this pull request.');
-            return;
-        }
-        // regex from @alex
-        const ASANA_TASK_LINK_REGEX = /https:\/\/app.asana.com\/(\d+)\/(?<project>\d+)\/(?<taskId>\d+).*/gi;
-        const match = ASANA_TASK_LINK_REGEX.exec(description);
-        const taskId = core.getInput('task-id');
-        const task = await client.tasks.findById(taskId);
-        if (!task) {
-            core.setFailed('Task not found');
-            return;
-        }
-        task.notes = description;
+    const ASANA_SECRET = core.getInput('asana-secret');
+    // Get project and task id from the PR description # thanks Alex
+    const ASANA_TASK_LINK_REGEX = /https:\/\/app.asana.com\/(\d+)\/(?<project>\d+)\/(?<taskId>\d+).*/gi;
+    const prInfo = github.context.payload;
+    if (!prInfo.pull_request) {
+        core.setFailed('No pull request found.');
+        return;
     }
-    catch (error) {
-        core.setFailed(error.message);
+    const description = prInfo.pull_request.body;
+    if (!description) {
+        core.setFailed('No description found for this pull request.');
+        return;
+    }
+    // rightfully stolen from @alex who stolen from https://github.com/seniorly/Abott/blob/master/src/lib/git.js
+    let taskIds = [], rawParseUrlTask;
+    while ((rawParseUrlTask = ASANA_TASK_LINK_REGEX.exec(description)) !== null) {
+        if (rawParseUrlTask.groups) {
+            taskIds.push(rawParseUrlTask.groups.taskId);
+        }
+    }
+    if (taskIds.length === 0) {
+        core.setFailed('No task id found in the description.');
+        return;
+    }
+    const asana = Asana.Client.create({
+        defaultHeaders: { 'asana-enable': 'string_ids,new_sections' },
+        clientSecret: ASANA_SECRET
+    });
+    for (let taskId of taskIds) {
+        const task = await asana.tasks.findById(taskId);
+        console.log(task);
     }
 }
 
